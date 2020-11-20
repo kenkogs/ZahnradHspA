b6380d019dfafc6f7048c2605831303292e5a081
Detmer1990 <72165415+Detmer1990@users.noreply.github.com>
2020-11-20 10:30:45 +0100

    Initial Commit

---
+++ ZahnradGUI/zahnrad/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahnrad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c, d, e, h, f, g, i, j, k, l, m, n;
+            Console.WriteLine("a=Modul,b=Zaehnezahl,c=Teilkreisdurchmesser,d=Breite,");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("e=Kopfspiel,f=zahnfusshoehe,g=zahnkopfhoehe");
+            e = 0.167 * a;
+            h = 2 * a + e;
+            f = a + e;
+            g = a;
+            Console.WriteLine("e=");
+            Console.WriteLine(e);
+            Console.WriteLine("f=");
+            Console.WriteLine(f);
+            Console.WriteLine("g=");
+            Console.WriteLine(g);
+
+            Console.WriteLine("i=Teilung");
+            i = Math.PI * a;
+            Console.WriteLine("i=");
+            Console.WriteLine(i);
+
+            Console.WriteLine("j=Fusskreisdurchmesser1,n=Fusskreisdurchmesser2");
+            j = c - 2 * (a + e);
+            n = c + 2 * (a + e);
+            Console.WriteLine("j=");
+            Console.WriteLine(j);
+            Console.WriteLine("n=");
+            Console.WriteLine(n);
+
+            Console.WriteLine("k=Grundkreisdurchmesser1,l=Grundkreisdurchmesser2,m=Grundkreisdurchmesser3");
+            k = c + 2 * a;
+            l = a * (b + 2);
+            m = a * (b - 2);
+            Console.WriteLine("k=");
+            Console.WriteLine(k);
+            Console.WriteLine("l=");
+            Console.WriteLine(l);
+            Console.WriteLine("m=");
+            Console.WriteLine(m);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ZahnradGUI/zahnrad/zahnrad/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zahnrad
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Klicken Sie auf die Schaltfläche, um das Klickereignis auszulösen
+        private void Bt_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            Double a, b, c, d, e1, h, f, g, i, j, k, n;         //Definieren Sie Variablen vom Typ double zum Empfangen von Daten und zum Berechnen von Daten
+            a = Convert.ToDouble(text_a.Text.ToString());       //a变量接收文本框的值，并且需要把文本框的字符串类型的值转成double类型
+            b = Convert.ToDouble(text_b.Text.ToString());
+            c = Convert.ToDouble(text_c.Text.ToString());
+            d = Convert.ToDouble(text_d.Text.ToString());
+
+            e1 = 0.167 * a;//Definieren e=0,167*a.              //rechnen
+            h = 2 * a + e1;//Definieren h=2*a+e.
+            f = a + e1;//Definieren f=a+e.
+            g = a;//Definieren g=a.
+            text_e.Text = e1.ToString();//Weisen Sie das berechnete Ergebnis textbox, textboxfor, textboxg zu und konvertieren Sie es in String typ
+            text_f.Text = f.ToString();
+            text_g.Text = g.ToString();
+
+            i = Math.PI * a;                                            
+            text_i.Text = i.ToString();
+            j = c - 2 * (a + e1);//Definieren j=c - 2 * (a + e).
+            n = c + 2 * (a + e1);
+            text_j.Text = j.ToString();
+            text_n.Text = n.ToString();
+
+            k = a * b * Math.Cos(20 * Math.PI / 180);
+            text_k.Text = k.ToString();
+
+        }
+    }
+}
