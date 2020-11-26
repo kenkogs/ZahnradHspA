36bff3d0a3d02e2dd61da3ca88bbdfdd7a9609e9
ei2131 <56160507+ei2131@users.noreply.github.com>
2020-11-26 10:19:01 +0100

    initial commit

--- ZahnradGUI/zahnrad/MainWindow.xaml.cs
+++ ZahnradGUI/zahnrad/MainWindow.xaml.cs
@@ -27,62 +27,78 @@ namespace zahnrad
        
         private void Bt_calculate_Click(object sender, RoutedEventArgs e)
         {
-            Double a, b, c, d, e1, h, f, g, i, j, k, n;                
-            if (radio_Geradzahnrad.IsChecked == true)
+            Double a, b, c, d, e1, h, f, g, i, j, k, n;
+            
+            try
             {
-                a = Convert.ToDouble(text_a.Text.ToString());               
-                b = Convert.ToDouble(text_b.Text.ToString());
-                c = Convert.ToDouble(text_c.Text.ToString());
-                d = Convert.ToDouble(text_d.Text.ToString());
+                a = Convert.ToDouble(tb_a.Text.ToString());                         // Eingabe von Werten a - d in TextBoxen 
+                b = Convert.ToDouble(tb_b.Text.ToString());
+                c = Convert.ToDouble(tb_c.Text.ToString());
+                d = Convert.ToDouble(tb_d.Text.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Bitte geben Sie positive reale Werte ein");        // Konsole ploppt auf falls Buchstaben oder negative Werte eingegeben werden.
+                return;
+            }
+            if (a < 0 || b < 0 || c < 0 || d < 0)
+            {
+                MessageBox.Show("Bitte geben Sie positive reale Werte ein");
+                return;
+            } 
 
-                e1 = 0.167 * a;//Definieren e=0,167*a.                     
-                h = 2 * a + e1;//Definieren h=2*a+e.
-                f = a + e1;//Definieren f=a+e.
-                g = a;//Definieren g=a.
-                text_e.Text = e1.ToString();                                
-                text_f.Text = f.ToString();
-                text_g.Text = g.ToString();
+            if(radio_Geradzahnrad.IsChecked == true)
+
+            {   e1 = 0.167 * a;                                                     //Definierung von e=0,167*a.                     
+                h = 2 * a + e1;                                                     //Definierung von  h=2*a+e.
+                f = a + e1;                                                         //Definierung von f=a+e.
+                g = a;                                                              //Definierung von g=a.
+                tb_e.Text = e1.ToString();                                
+                tb_f.Text = f.ToString();
+                tb_g.Text = g.ToString();
 
                 i = Math.PI * a;
-                text_i.Text = i.ToString();
-                j = c - 2 * (a + e1);//Definieren j=c - 2 * (a + e).
+                tb_i.Text = i.ToString();
+                j = c - 2 * (a + e1);                                               //Definierung von j=c - 2 * (a + e).
                 n = a * (b+2);
-                text_j.Text = j.ToString();
-                text_n.Text = n.ToString();
+                tb_j.Text = j.ToString();
+                tb_n.Text = n.ToString();
 
                 k = a * b * Math.Cos(20 * Math.PI / 180);
-                text_k.Text = k.ToString();
+                tb_k.Text = k.ToString();
             }
             else if(radio_Innenzahnrad.IsChecked == true)
             {
-                a = Convert.ToDouble(text_a.Text.ToString());              
-                b = Convert.ToDouble(text_b.Text.ToString());
-                c = Convert.ToDouble(text_c.Text.ToString());
-                d = Convert.ToDouble(text_d.Text.ToString());
+                a = Convert.ToDouble(tb_a.Text.ToString());              
+                b = Convert.ToDouble(tb_b.Text.ToString());
+                c = Convert.ToDouble(tb_c.Text.ToString());
+                d = Convert.ToDouble(tb_d.Text.ToString());
 
-                e1 = 0.167 * a;//Definieren e=0,167*a.                    
-                h = 2 * a + e1;//Definieren h=2*a+e.
-                f = a + e1;//Definieren f=a+e.
-                g = a;//Definieren g=a.
-                text_e.Text = e1.ToString();                                
-                text_f.Text = f.ToString();
-                text_g.Text = g.ToString();
+                
+                e1 = 0.167 * a;                                                     //Definierung von e=0,167*a.                    
+                h = 2 * a + e1;                                                     //Definierung von h=2*a+e.
+                f = a + e1;                                                         //Definierung von f=a+e.
+                g = a;                                                              //Definierung von g=a.
+                tb_e.Text = e1.ToString();                                
+                tb_f.Text = f.ToString();
+                tb_g.Text = g.ToString();
 
                 i = Math.PI * a;
-                text_i.Text = i.ToString();
-                j = c + 2 * (a + e1);//Definieren j=c - 2 * (a + e).
+                tb_i.Text = i.ToString();
+                j = c + 2 * (a + e1);                                               //Definierung von j=c - 2 * (a + e).
                 n = a *(b-2);
-                text_j.Text = j.ToString();
-                text_n.Text = n.ToString();
+                tb_j.Text = j.ToString();
+                tb_n.Text = n.ToString();
 
                 k = a * b * Math.Cos(20 * Math.PI / 180);
-                text_k.Text = k.ToString();
+              
+                tb_k.Text = k.ToString();
             }
             
 
         }
 
-        private void Radio_Geradzahnrad_Checked(object sender, RoutedEventArgs e)
+        private void Radio_Geradzahnrad_Checked(object sender, RoutedEventArgs e)   // JPEG Stirnzahnrad
         {
             Uri uri = new Uri(@"geradzahnrad.jpg", UriKind.Relative);
             image_kind.Source = new BitmapImage(uri);
@@ -90,9 +106,14 @@ namespace zahnrad
 
         private void Radio_Innenzahnrad_Checked(object sender, RoutedEventArgs e)
         {
-            Uri uri = new Uri(@"innenzahnrad.jpg", UriKind.Relative);
+            Uri uri = new Uri(@"innenzahnrad.jpg", UriKind.Relative);               // JPEG Innenzahnrad
             image_kind.Source = new BitmapImage(uri);
  
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)                 // Ende btn
+        {
+            this.Close();
         }
     }
 }
